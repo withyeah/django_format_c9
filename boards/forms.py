8c6ebf38f833bcd81532b2ae469a5f86345ae9fa
withyeah <areygim@gmail.com>
2019-03-21 07:06:06 +0000

    03_use ModelForm

---
+++ boards/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from .models import Board
+
+# class BoardForm(forms.Form):
+#     title = forms.CharField(label='제목', widget=forms.TextInput(attrs={
+#                                                     'placeholder': 'THE TITLE',
+                                                       
+#                                             }))
+#     content = forms.CharField(label='내용', 
+#                             error_messages={'required': '제발 내용을 입력해주세요!'},        
+#                             widget=forms.Textarea(attrs={
+#                                                     'class': 'Content-input',
+#                                                     'rows': 5,
+#                                                     'cols': 50,
+#                                                     'placeholder': 'Fill the Content',
+#                                             }))
+    
+class BoardForm(forms.ModelForm):
+    # Model의 정보를 작성하는 곳
+    class Meta:
+        model = Board
+        # models.py에 지정해놓은 필드를 사용한다.
+        # fields, widget은 정해진 변수명
+        fields = '__all__'
+        widgets = {'title': forms.TextInput(attrs={
+                                                'placeholder': '제목을 입력하세요.',
+                                                'class': 'title'}),
+                  'content': forms.Textarea(attrs={
+                                                'placeholder': '내용을 입력하세요.',
+                                                'class': 'content',
+                                                'rows': 5,
+                                                'cols': 50,})}
+        error_messages = {'title': {
+                                        'required': '제발 입력해주세요.'
+                                    },
+                          'content': {
+                                        'required': '내용을 입력하지 않으면 죽는다.'
+                                    },
+        }
--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Board
+from .forms import BoardForm
 
 # Create your views here.
 
@@ -10,19 +11,92 @@ def index(request):
     }
     return render(request, 'boards/index.html', context)
     
+# def create(request):
+#     # POST 요청이면 FORM 데이터를 처리한다.
+#     if request.method == 'POST':
+#         # 이 처리과정은 "binding" 으로 불리는데, 폼의 유효성 체크를 할 수 있도록 해준다.
+#         form = BoardForm(request.POST)
+#         # form 유효성 체크 (안전한 데이터인지 체크)
+#         if form.is_valid():
+#             title = form.cleaned_data.get('title')
+#             content = form.cleaned_data.get('content')
+#             # 검증을 통과한 깨끗한 데이터를 form에서 가져와서 board를 만든다.
+#             board = Board.objects.create(title=title, content=content)
+#             return redirect('boards:detail', board.pk)
+#     # GET 요청(혹은 다른 메서드)이면 기본 폼을 생성한다.
+#     else:
+#         form = BoardForm()
+#     context = {
+#         'form': form,
+#     }
+#     return render(request, 'boards/create.html', context)
+    
 def create(request):
+    # POST 요청이면 FORM 데이터를 처리한다.
     if request.method == 'POST':
-        title = request.POST.get('title')
-        content = request.POST.get('content')
-        board = Board(title=title, content=content)
-        board.save()
-        return redirect('boards:index')
+        # 이 처리과정은 "binding" 으로 불리는데, 폼의 유효성 체크를 할 수 있도록 해준다.
+        form = BoardForm(request.POST)
+        # form 유효성 체크 (안전한 데이터인지 체크)
+        if form.is_valid():
+            board = form.save()
+            return redirect('boards:detail', board.pk)
+    # GET 요청(혹은 다른 메서드)이면 기본 폼을 생성한다.
     else:
-        return render(request, 'boards/create.html')
+        form = BoardForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'boards/form.html', context)
         
 def detail(request, board_pk):
-    board = Board.objects.get(pk=board_pk)
+    # board = Board.objects.get(pk=board_pk)
+    board = get_object_or_404(Board, pk=board_pk)
     context = {
         'board':board
     }
     return render(request, 'boards/detail.html', context)
+    
+def delete(request, board_pk):
+    board = get_object_or_404(Board, pk=board_pk)
+    if request.method == 'POST':
+        board.delete()
+        return redirect('boards:index')
+    else:
+        return redirect('boards:detail', board.pk)
+    
+# def update(request, board_pk):
+#     board = get_object_or_404(Board, pk=board_pk)
+#     if request.method == 'POST':
+#         form = BoardForm(request.POST)
+#         if form.is_valid():
+#             board.title = form.cleaned_data.get('title')
+#             board.content = form.cleaned_data.get('content')
+#             board.save()
+#             return redirect('boards:detail', board.pk)
+#     # GET 요청이면(수정하기 버튼을 눌렀을 때)
+#     else:
+#         # BoardForm을 초기화(사용자 입력 값을 넣어준 상태로)
+#         # form = BoardForm(initial={'title': board.title, 'content': board.content})
+#         form = BoardForm(initial=board.__dict__)
+#     # 1. POST : 요청에서 검증에 실패하였을 때, 오류 메세지가 포함된 상태로 넘어온다.
+#     # 2. GET : 요청에서 초기화된 상태로 넘어온다.
+#     context = {
+#         'form': form,
+#     }
+#     return render(request, 'boards/create.html', context)
+    
+def update(request, board_pk):
+    board = get_object_or_404(Board, pk=board_pk)
+    if request.method == 'POST':
+        form = BoardForm(request.POST, instance=board)    # 1
+        if form.is_valid():                               
+            board = form.save()                           # 2
+            return redirect('boards:detail', board.pk)
+    else:
+        form = BoardForm(instance=board)                  # 3
+    context = {
+        'form': form,
+        'board': board,
+    }
+    return render(request, 'boards/form.html', context)
+        
